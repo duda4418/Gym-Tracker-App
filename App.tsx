0c35141a244a6fa06824fa71b99fa71fd8f612f0
duda4418 <146849849+duda4418@users.noreply.github.com>
2025-03-15 20:55:21 +0200

    Adding expo extensions and tailwind for nativea react

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={tw`flex-1 bg-white items-center justify-center`}>
+      <Text style={tw`text-red-500`}>Open up App.tsx to start working on your app!</Text>
+      <Text style={tw`text-green-700`}>BUNA BA IoN</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
