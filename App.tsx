b2f4bec7ce56bfe92927003b37c7bca15fc1cf6c
duda4418 <146849849+duda4418@users.noreply.github.com>
2025-03-15 22:40:51 +0200

    Adding React Native Paper

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import tw from 'tailwind-react-native-classnames';
+import React from 'react';
+import { PaperProvider } from 'react-native-paper';
+import { Button } from 'react-native-paper';
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={tw`flex-1 bg-white items-center justify-center`}>
-      <Text style={tw`text-red-500`}>Open up App.tsx to start working on your app!</Text>
-      <Text style={tw`text-green-700`}>BUNA BA IN</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Button mode="contained" onPress={() => console.log('Pressed')}>
+          Press me
+        </Button>
+      </View>
+    </PaperProvider>
   );
 }
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { StatusBar } from 'expo-status-bar';
+// import { Text, View } from 'react-native';
+// import tw from 'tailwind-react-native-classnames';
+
+// export default function App() {
+//   return (
+//     <View style={tw`flex-1 bg-white items-center justify-center`}>
+//       <Text style={tw`text-red-500`}>Open up App.tsx to start working on your app!</Text>
+//       <Text style={tw`text-green-700 bg-red-300`}>BUNIN</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+//   AppRegistry.registerComponent(appName, () => Main);
+// }
