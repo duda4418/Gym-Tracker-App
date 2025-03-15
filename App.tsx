cb67f7349feb10a177cf8d0bef8dc409e5cfbf8c
duda4418 <146849849+duda4418@users.noreply.github.com>
2025-03-15 22:01:40 +0200

    Adding Android Studio emulator

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={tw`flex-1 bg-white items-center justify-center`}>
       <Text style={tw`text-red-500`}>Open up App.tsx to start working on your app!</Text>
-      <Text style={tw`text-green-700`}>BUNA BA IoN</Text>
+      <Text style={tw`text-green-700`}>BUNA BA IN</Text>
       <StatusBar style="auto" />
     </View>
   );
